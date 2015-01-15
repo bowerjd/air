4cb01b91ea6e67b8fdc659881dc546d0c9e1a85f
James Bodkin <jbodkin@caci.co.uk>
2015-01-15 10:33:02 +0000

    Add javadoc to CompilerException

--- air-asset/src/main/java/com/lonelystorm/air/asset/exceptions/CompilerException.java
+++ air-asset/src/main/java/com/lonelystorm/air/asset/exceptions/CompilerException.java
@@ -1,17 +1,26 @@
 package com.lonelystorm.air.asset.exceptions;
 
-
+/**
+ * Exception thrown by Compilers if an error occurs whilst attempting
+ * to compile the source asset.
+ */
 public class CompilerException extends Exception {
 
     /**
-     * Default Serial Version
+     * Serial Version.
      */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * {@inheritDoc}
+     */
     public CompilerException(String message) {
         super(message);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public CompilerException(String message, Throwable cause) {
         super(message, cause);
     }
